e408d8167494a7637092538bef6af423f6e9843a
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-07-14 13:37:13 +0200

    Add main application, add a few method to handling the database

---
+++ ch22/PodMain.hs
@@ -0,0 +1,74 @@
+module Main where
+
+import PodDownload
+import PodDB
+import PodTypes
+import System.Environment
+import Database.HDBC
+import Network.Socket(withSocketsDo)
+
+main = withSocketsDo $ handleSqlError $ do
+    args <- getArgs
+    dbh <- connect "pod.db"
+    case args of
+        ["add", url] -> add dbh url
+        ["list"] -> list dbh
+        ["update"] -> update dbh
+        ["download"] -> download dbh
+        ["remove", id] -> remove dbh (read id :: Integer)
+        ["fetch"] -> do
+            update dbh
+            download dbh
+        _ -> syntaxError
+    disconnect dbh
+
+add dbh url = do
+    addPodcast dbh pc
+    commit dbh
+    where
+        pc = Podcast {castId = 0, castURL = url}
+
+remove dbh id = do
+    removePodcast dbh $ podcastForId id
+    commit dbh
+
+podcastForId :: Integer -> Podcast
+podcastForId id = Podcast { castId = id, castURL = "" }
+
+list dbh = do
+    pclist <- getPodcasts dbh
+    mapM_ displayPodcast pclist
+    where
+        displayPodcast podcast = putStrLn $ show (castId podcast) ++ " => " ++ castURL podcast
+
+update dbh = do
+    pclist <- getPodcasts dbh
+    mapM_ procPodcast pclist
+    where
+        procPodcast pc = do
+            putStrLn $ "Updating from " ++ (castURL pc)
+            updatePodcastFromFeed dbh pc
+
+download dbh = do
+    pclist <- getPodcasts dbh
+    mapM_ procPodcast pclist
+    where
+        procPodcast pc = do
+            putStrLn $ "Considering " ++ (castURL pc)
+            episodelist <- getPodcastEpisodes dbh pc
+            let dleps = filter (\ep -> epDone ep == False) episodelist
+            mapM_ procEpisode dleps
+            where
+                procEpisode ep = do
+                    putStrLn $ "Downloading " ++ (epURL ep)
+                    getEpisode dbh ep
+
+syntaxError = putStrLn
+    "Usage: pod command [args\n\
+    \\n\
+    \pod add url    Adds a new podcast with the given URL\n\
+    \pod download   Downloads all pending episodes\n\
+    \pod fetch      Updates, then downloads\n\
+    \pod list       List podcasts\n\
+    \pod remove id  Remove the given podcast\n\
+    \pod update     Downloads podcast feeds, looks for new episodes\n"
